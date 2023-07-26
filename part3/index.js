00afb585014e9ac07379bc423df5cd0ede7802bc
CanIBuyYouAHat <cironoc5@gmail.com>
2023-07-25 21:47:43 -0400

    Oops[A

--- part3/index.js
+++ part3/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 morgan.token('content', function (request) { return JSON.stringify(request.body) })
 
-app.use('cors')
+app.use(cors)
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
